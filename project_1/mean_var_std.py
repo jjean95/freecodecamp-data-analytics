73fb2253e3a88d4e22ebbf64c53f2007aeeb3907
Goh Jia Ean <44393280+jjean95@users.noreply.github.com>
2022-10-06 10:39:08 +0800

    Update mean_var_std.py

--- project_1/mean_var_std.py
+++ project_1/mean_var_std.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def calculate(list):
   if len(list) != 9:
-    raise ValueError("List must contain nine numbers.")
+    raise ValueError("List must contain nine numbers.") #to eliminate any list with less than 9 numbers
       
   ls = np.array(list).reshape(3,3)
   cal_mean_ax0 = ls.mean(axis=0).tolist()
