e38a7828c8ff7c121ac184022953754b4981f653
Goh Jia Ean <44393280+jjean95@users.noreply.github.com>
2022-10-07 13:07:42 +0800

    Update mean_var_std.py

--- project_1/mean_var_std.py
+++ project_1/mean_var_std.py
@@ -5,16 +5,22 @@ def calculate(list):
     raise ValueError("List must contain nine numbers.") #to eliminate any list with less than 9 numbers
       
   ls = np.array(list).reshape(3,3) #change list into an array, and reshape it into 3X3
+  #calculate mean for axis 0 and axis 1
   cal_mean_ax0 = ls.mean(axis=0).tolist()
   cal_mean_ax1 = ls.mean(axis=1).tolist()
+  #calculate variance for axis 0 and axis 1
   cal_var_ax0 = ls.var(axis=0).tolist()
   cal_var_ax1 = ls.var(axis=1).tolist()
+  #calculate standard deviation for axis 0 and axis 1
   cal_std_ax0 = ls.std(axis=0).tolist()
   cal_std_ax1 = ls.std(axis=1).tolist()
+  #calculate maximum for axis 0 and axis 1
   cal_max_ax0 = ls.max(axis=0).tolist()
   cal_max_ax1 = ls.max(axis=1).tolist()
+  #calculate minimum for axis 0 and axis 1
   cal_min_ax0 = ls.min(axis=0).tolist()
   cal_min_ax1 = ls.min(axis=1).tolist()
+  #calculate sum for axis 0 and axis 1
   cal_sum_ax0 = ls.sum(axis=0).tolist()
   cal_sum_ax1 = ls.sum(axis=1).tolist()
   
